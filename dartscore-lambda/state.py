c59bd7b194d8ee5601884a8d5567ac5f7fa035ef
Darius Cox <ddcgamer243@gmail.com>
2021-01-24 16:30:50 -0500

    update gamestate

--- dartscore-lambda/state.py
+++ dartscore-lambda/state.py
@@ -63,3 +63,44 @@ def get_game_state(event, context):
     }
     print(response)
     return response
+
+def update_game_state(event, context):
+    body = json.loads(event['body'])
+    game_id = body.get('game_id')
+    player = body.get('player')
+    player_score = body.get('player_score')
+
+    game = table.get_item(Key={'game_id': game_id})['Item']
+    game_state = game['game_state']
+
+    game_state[player] = player_score
+
+    try:
+        table.update_item(
+            Key={'game_id': game_id},
+            UpdateExpression="set game_state = :gs",
+            ExpressionAttributeValues={
+                ':gs': game_state,
+                },  
+        ReturnValues="UPDATED_NEW"
+        )
+        logger.info(
+            "Updated Scores for Game: %s, Player: %s ", game_id, player)
+        body = {
+            "state": "updated",
+            "game_state": game_state
+        }
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(body, default=handle_decimal_type),
+            "headers": {
+                    "Access-Control-Allow-Origin" : "*", # Required for CORS support to work
+                    "Access-Control-Allow-Credentials" : True # Required for cookies, authorization headers with HTTPS
+                }
+        }
+        return response
+    except ClientError:
+        logger.exception(
+            "Couldn't initialize gamestate for Game: %s ", game_id)
+        status_code = 503
+        return status_code
