176a086c75b9972b1dd1f6775eede59cf930d1f6
Darius Cox <ddcgamer243@gmail.com>
2021-01-28 01:44:03 -0500

    state cors

--- dartscore-lambda/state.py
+++ dartscore-lambda/state.py
@@ -16,6 +16,15 @@ logger.setLevel(logging.INFO)
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('dartscore')
 
+def _get_allowed_origin(event):
+    origin = event.get("headers", {}).get("origin", None)
+    logger.info("Origin is: %s", origin)
+
+    if origin and (origin == 'https://www.danielcox.software' or origin == 'http://localhost:3000'):
+        return origin
+
+    return None
+
 def handle_decimal_type(obj):
   if isinstance(obj, Decimal):
       if float(obj).is_integer():
@@ -25,6 +34,13 @@ def handle_decimal_type(obj):
   raise TypeError
 
 def lobby(event, context):
+    response = {"statusCode":200}
+    allowed_origin = _get_allowed_origin(event)
+    if allowed_origin:
+        response["headers"] = {
+            "Access-Control-Allow-Origin": allowed_origin,
+            "Access-Control-Allow-Credentials": True,
+        }
     game_id = event.get('queryStringParameters').get('game')
     logger.info(game_id)
     game = table.get_item(Key={'game_id': game_id})['Item']
@@ -33,18 +49,18 @@ def lobby(event, context):
     body = {
             "players": players,
             }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body),
-        "headers": {
-            "Access-Control-Allow-Origin" : "*", # Required for CORS support to work
-            "Access-Control-Allow-Credentials" : True # Required for cookies, authorization headers with HTTPS
-            }
-    }
+    response["body"] = json.dumps(body)
     print(response)
     return response
 
 def get_game_state(event, context):
+    response = {"statusCode":200}
+    allowed_origin = _get_allowed_origin(event)
+    if allowed_origin:
+        response["headers"] = {
+            "Access-Control-Allow-Origin": allowed_origin,
+            "Access-Control-Allow-Credentials": True,
+        }
     game_id = event.get('queryStringParameters').get('game')
     logger.info(game_id)
     game = table.get_item(Key={'game_id': game_id})['Item']
@@ -53,27 +69,21 @@ def get_game_state(event, context):
     body = {
             "game_state": game_state,
             }
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body, default=handle_decimal_type),
-        "headers": {
-            "Access-Control-Allow-Origin" : "*", # Required for CORS support to work
-            "Access-Control-Allow-Credentials" : True # Required for cookies, authorization headers with HTTPS
-            }
-    }
+    response["body"] = json.dumps(body, default=handle_decimal_type)
     print(response)
     return response
 
 def update_game_state(event, context):
+    response = {"statusCode":200}
+    allowed_origin = _get_allowed_origin(event)
+    if allowed_origin:
+        response["headers"] = {
+            "Access-Control-Allow-Origin": allowed_origin,
+            "Access-Control-Allow-Credentials": True,
+        }
     body = json.loads(event['body'])
     game_id = body.get('game_id')
-    # player = body.get('player')
     game_state = body.get('game_state')
-
-    # game = table.get_item(Key={'game_id': game_id})['Item']
-    # game_state = game['game_state']
-
-    # game_state[player] = player_score
 
     try:
         table.update_item(
@@ -90,14 +100,7 @@ def update_game_state(event, context):
             "state": "updated",
             "game_state": game_state
         }
-        response = {
-            "statusCode": 200,
-            "body": json.dumps(body, default=handle_decimal_type),
-            "headers": {
-                    "Access-Control-Allow-Origin" : "*", # Required for CORS support to work
-                    "Access-Control-Allow-Credentials" : True # Required for cookies, authorization headers with HTTPS
-                }
-        }
+        response["body"] = json.dumps(body, default=handle_decimal_type)
         return response
     except ClientError:
         logger.exception(
